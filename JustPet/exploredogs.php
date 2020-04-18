9b46b014c0311373093a930bfc303109a1317182
Harsha Hirushan <hirushanharsha4@gmail.com>
2020-04-18 22:05:57 +0530

    pet project

---
+++ JustPet/exploredogs.php
@@ -0,0 +1,116 @@
+<head>
+    <meta charset="utf-8">
+    <title>Pawrex Template | Services</title>
+    <!-- Stylesheets -->
+    <link href="css/css//bootstrap.css" rel="stylesheet">
+    <link href="css/css//style.css" rel="stylesheet">
+    <link href="css/css//responsive.css" rel="stylesheet">
+    <!--Color Switcher Mockup-->
+    <link href="css/css//color-switcher-design.css" rel="stylesheet">
+    <!--Color Themes-->
+    <link id="theme-color-file" href="css/css//color-themes/default-theme.css" rel="stylesheet">
+
+    <!-- Responsive -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+</head>
+<body>
+
+  <section class="services-section-four">
+             <div class="auto-container">
+                 <h2>Dogs</h2>
+                 <div class="row clearfix">
+                     <!--Services Block-->
+                     <div class="services-block col-md-6 col-sm-6 col-xs-12">
+                         <div class="inner-box">
+                             <div class="content">
+                                 <div class="icon-box">
+
+                                  		<img src="./img/dog1.jpg" alt="">
+                                 </div>
+                                 <h3>German Shepherd</a></h3>
+
+                                 <div class="text">
+                                   <br><br><br><br><br>
+                                   <b>German Shepard got originated from Germany. This is a new breed of dog that originated in the year 1899. These are called as working dogs which are developed for herding sheep. A German dog has a lot of strength, obedient, intelligent and very well trained. It is called a second-most popular breed of dog in the United States and fourth-most popular in the United Kingdom.The breed height starts at 60-65 cm for males and for females height was 55-60 cm. They mostly observe in tan/black and red/black. They have double coat. Outer coat sheds all round and thick undercoat is very densed. Life span of German Shepard is 10.5 years.</div>
+                            </b> </div>
+                         </div>
+                     </div>
+                     <!--Services Block-->
+                     <div class="services-block col-md-6 col-sm-6 col-xs-12">
+                         <div class="inner-box">
+                             <div class="content">
+                                 <div class="icon-box">
+                                     <img src="./img/dog2.jpg" alt="">
+                                 </div>
+
+                                 <h3>Bulldog</h3>
+                                 <div class="text">
+                                            <br><br><br><br><br>
+                                   <b>Country of origin of a bulldog is from England and the United Kingdom. Bulldogs are even called British Bulldog and English bulldog. The average life span of a bulldog is 07 to 10 years. They have very small nasal cavities and are sensitive to heat. Its height is about 12-16 inches. Weight of male ranges from 53-55 pounds and females 49-51 pounds. These types of dogs are grown as pets. They need exercise like other dogs.</div>
+                          </b>   </div>
+                         </div>
+                     </div>
+                     <!--Services Block-->
+                     <div class="services-block col-md-6 col-sm-6 col-xs-12">
+                         <div class="inner-box">
+                             <div class="content">
+                                 <div class="icon-box">
+                                    <img src="./img/dog3.jpg" alt="">
+                                 </div>
+                                 <h3>Golden Retriever</h3>
+                                 <div class="text">
+                                <br><br><br><br><br>
+                                  <b>Golden retriever is originated from Scotland, United Kingdom and England. It is very reliable, trustworthy, kind and intelligent. Its height is for female varies from 55-57cm and male 58-61cm. Weight of male 29.5-34 kg and for female 25-32 kg. These are used as guide dogs. Life span is about 11-12 years.</div>
+                          </b>    </div>
+                         </div>
+                     </div>
+                     <!--Services Block-->
+                     <div class="services-block col-md-6 col-sm-6 col-xs-12">
+                         <div class="inner-box">
+                             <div class="content">
+                                 <div class="icon-box">
+                                      <img src="./img/dog4.jpg" alt="">
+                                 </div>
+                                 <h3>Poodle</h3>
+                                 <div class="text">
+<br><br><br><br><br>
+                                <b>hese are mostly seen in France and Germany. It is very active, alert, intelligent, faithful and even trained. Height ranges from 35-45cm. It is water type dog. Poodles worn Best in show in 1966 and 1982 and even awarded. These types of pet dogs are available in multiple colors. Life span is about 12-15 years.</div>
+                      </b>   </div>
+                         </div>
+                     </div>
+                     <!--Services Block-->
+                     <div class="services-block col-md-6 col-sm-6 col-xs-12">
+                         <div class="inner-box">
+                             <div class="content">
+                                 <div class="icon-box">
+                                   <img src="./img/dog5.jpg" alt="">
+
+                                 </div>
+                                 <h3>Shih Tzu</h3>
+                                 <div class="text">
+<br><br><br><br><br>
+                                <b>It got originated in china. Other names are Chinese Lion Dog and Chrysanthemum Dog. They are litter in size up to 1-8. Weight for both female and male varies from 8.8-16 lbs. Height also same for both ranges from 7.9-11 inches. It is available in some colors like brindle red, gold etc. These are playful, outgoing, loyal and gentle. Life span of Shi is 10-16 years.</div>
+                          </b>      </div>
+                         </div>
+                     </div>
+                     <!--Services Block-->
+                     <div class="services-block col-md-6 col-sm-6 col-xs-12">
+                         <div class="inner-box">
+                             <div class="content">
+                                 <div class="icon-box">
+                                       <img src="./img/dog6.jpg" alt="">
+                                 </div>
+                                 <h3>Pug Dog</h3>
+                                 <div class="text">
+                                   <br><br><br><br><br>
+                                  <b>These are originated in china and when migrated to Europe in sixteenth century it was well popularized. It was developed as passion by Queen Victoria in 19th century. Likewise it was passed to royal family. These types(1) of dogs are strong and aggressive and suitable to grow at houses. These have lazy nature. Its height is of 30cm. Life span is of 12-15 years.</div>
+                            </b>  </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </section>
+
+</body>
