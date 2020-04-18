e385e7e1089a319760481498b022ea8f284f5dc9
Harsha Hirushan <hirushanharsha4@gmail.com>
2020-04-18 22:54:47 +0530

    cats added

--- JustPet/explorecats.php
+++ JustPet/explorecats.php
@@ -27,13 +27,13 @@
                              <div class="content">
                                  <div class="icon-box">
 
-                                  		<img src="./img/dog1.jpg" alt="">
+                                  		<img src="./img/cat1.jpg" alt="">
                                  </div>
-                                 <h3>German Shepherd</a></h3>
+                                 <h3>Siamese</a></h3>
 
                                  <div class="text">
                                    <br><br><br><br><br>
-                                   <b>German Shepard got originated from Germany. This is a new breed of dog that originated in the year 1899. These are called as working dogs which are developed for herding sheep. A German dog has a lot of strength, obedient, intelligent and very well trained. It is called a second-most popular breed of dog in the United States and fourth-most popular in the United Kingdom.The breed height starts at 60-65 cm for males and for females height was 55-60 cm. They mostly observe in tan/black and red/black. They have double coat. Outer coat sheds all round and thick undercoat is very densed. Life span of German Shepard is 10.5 years.</div>
+                                   <b>Popular since the 19th century, this breed of cat originated in Thailand (formerly known as Siam). The Siamese has helped create many other breeds, including the Oriental Shorthair, Sphynx, and Himalayan.Siamese cats have distinct markings called "points" that are the areas of coloration on their face, ears, feet, and tail, but some Siamese may not have any points. There are also two varieties of Siamese cats, one with an "apple" shaped head and chubbier body and one with a larger head and slender body.The animated Disney film, "Lady and the Tramp," featured Siamese cats singing a song and demonstrating not only their intelligence but also how vocal they can be.Many Siamese cats that were originally from Thailand had a kink in their tail. This trait was bred out of most Siamese, but you'll still find it in Siamese street cats in Thailand.</div>
                             </b> </div>
                          </div>
                      </div>
@@ -42,13 +42,15 @@
                          <div class="inner-box">
                              <div class="content">
                                  <div class="icon-box">
-                                     <img src="./img/dog2.jpg" alt="">
+                                     <img src="./img/cat2.jpg" alt="">
                                  </div>
 
-                                 <h3>Bulldog</h3>
+                                 <h3>Persian</h3>
                                  <div class="text">
                                             <br><br><br><br><br>
-                                   <b>Country of origin of a bulldog is from England and the United Kingdom. Bulldogs are even called British Bulldog and English bulldog. The average life span of a bulldog is 07 to 10 years. They have very small nasal cavities and are sensitive to heat. Its height is about 12-16 inches. Weight of male ranges from 53-55 pounds and females 49-51 pounds. These types of dogs are grown as pets. They need exercise like other dogs.</div>
+                                   <b>Also lovingly referred to as one of the "smushed-face" cats, Persians have beautiful, long, fur coats. They can come in almost any color, depending on which cat registry association you are looking at, and have a flat face when compared to most other breeds of cats. Some breed descriptions even state the nose is not allowed to protrude past the eyes when viewed in a profile.
+Persians have been seen in many movies, art, commercials, and homes throughout the years and have also been one of the most popular breeds of cats for decades.
+Their coats demand regular grooming, otherwise, matting will result. Like many purebreds, they are prone to a variety of diseases, including renal and cardiac problems.Persians are also known as the Persian Longhair, the Iranian cat, and the Shirazi cat, named after the city of Shiraz, Iran, since they were originally imported from Iran.</div>
                           </b>   </div>
                          </div>
                      </div>
@@ -57,12 +59,17 @@
                          <div class="inner-box">
                              <div class="content">
                                  <div class="icon-box">
-                                    <img src="./img/dog3.jpg" alt="">
+                                    <img src="./img/cat3.jpg" alt="">
                                  </div>
-                                 <h3>Golden Retriever</h3>
+                                 <h3>Maine Coon</h3>
                                  <div class="text">
                                 <br><br><br><br><br>
-                                  <b>Golden retriever is originated from Scotland, United Kingdom and England. It is very reliable, trustworthy, kind and intelligent. Its height is for female varies from 55-57cm and male 58-61cm. Weight of male 29.5-34 kg and for female 25-32 kg. These are used as guide dogs. Life span is about 11-12 years.</div>
+                                  <b>
+Known for their large stature and thick fur coats, the Maine Coon is a cat that is difficult to ignore. Hailing from the state of Maine and the state’s official cat, the Maine Coon is a gentle giant. They are great hunters and were popularized after the CFA recognized them as a purebred in the late 1970s. They remain one of the most popular cat breeds.
+
+Many Maine Coons have extra toes, a trait referred to as polydactylism, which makes their already large feet even larger. This is a great feature to have when hunting in the snow since large feet act as snowshoes.
+
+The classic Maine Coon coloration is a brown tabby, but this breed can come in almost any color.</div>
                           </b>    </div>
                          </div>
                      </div>
@@ -71,12 +78,14 @@
                          <div class="inner-box">
                              <div class="content">
                                  <div class="icon-box">
-                                      <img src="./img/dog4.jpg" alt="">
+                                      <img src="./img/cat4.jpg" alt="">
                                  </div>
-                                 <h3>Poodle</h3>
+                                 <h3>Ragdoll</h3>
                                  <div class="text">
 <br><br><br><br><br>
-                                <b>hese are mostly seen in France and Germany. It is very active, alert, intelligent, faithful and even trained. Height ranges from 35-45cm. It is water type dog. Poodles worn Best in show in 1966 and 1982 and even awarded. These types of pet dogs are available in multiple colors. Life span is about 12-15 years.</div>
+                                <b>Ragdoll cats get their name from their docile temperament. They seem to go limp when picked up, much like a rag doll. At one time, it was even thought that they couldn’t feel pain, but that is of course only a myth.
+
+Ragdolls look a lot like long-haired Siamese cats with their pointed color patterns. They also have distinctive blue eyes and dog-like personalities, following their owners around the house.</div>
                       </b>   </div>
                          </div>
                      </div>
@@ -85,14 +94,15 @@
                          <div class="inner-box">
                              <div class="content">
                                  <div class="icon-box">
-                                   <img src="./img/dog5.jpg" alt="">
+                                   <img src="./img/cat5.jpg" alt="">
 
                                  </div>
-                                 <h3>Shih Tzu</h3>
+                                 <h3>Bengal</h3>
                                  <div class="text">
 <br><br><br><br><br>
-                                <b>It got originated in china. Other names are Chinese Lion Dog and Chrysanthemum Dog. They are litter in size up to 1-8. Weight for both female and male varies from 8.8-16 lbs. Height also same for both ranges from 7.9-11 inches. It is available in some colors like brindle red, gold etc. These are playful, outgoing, loyal and gentle. Life span of Shi is 10-16 years.</div>
-                          </b>      </div>
+                                <b>Bengals are wild looking cats—literally. Their markings make them look more like they belong in the jungle than in your home, but they are domesticated cats nonetheless. They are talkative and require a lot of exercise.
+
+Many colors are allowed on Bengals, but their approved patterns only include spots and rosettes. Their name comes from the Asian leopard cat’s taxonomic name, Prionailurus bengalensis bengalensis, since Bengals were originally bred from both domestic cats and this wildcat. They get their beautiful patterns from this wildcat as well.  </b>      </div>
                          </div>
                      </div>
                      <!--Services Block-->
@@ -100,12 +110,13 @@
                          <div class="inner-box">
                              <div class="content">
                                  <div class="icon-box">
-                                       <img src="./img/dog6.jpg" alt="">
+                                       <img src="./img/cat6.jpg" alt="">
                                  </div>
-                                 <h3>Pug Dog</h3>
+                                 <h3>Abyssinian</h3>
                                  <div class="text">
                                    <br><br><br><br><br>
-                                  <b>These are originated in china and when migrated to Europe in sixteenth century it was well popularized. It was developed as passion by Queen Victoria in 19th century. Likewise it was passed to royal family. These types(1) of dogs are strong and aggressive and suitable to grow at houses. These have lazy nature. Its height is of 30cm. Life span is of 12-15 years.</div>
+                                  <b>Originally from Ethiopia (previously known as Abyssinia), the Abyssinian cat has a distinctive red/orange agouti coat. The agouti fur, which actually means it has bands of light and dark coloring on the individual pieces of hair, gives the Abyssinian its unique look.
+In addition to the most common color referred to as ruddy, Abyssinians can also come in cinnamon, fawn, and blue.</div>
                             </b>  </div>
                          </div>
                      </div>
